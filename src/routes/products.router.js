ebc1d279ace858766beac72078f7212841fffe90
Lucas Szymkow <lucasszymkow01@gmail.com>
2023-06-01 19:01:53 -0300

    Deleted FileSystem endpoints

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -10,7 +10,7 @@ const router = Router();
 //****MONGODB API:*****
 //*********************
 
-router.get("/db/", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     let { limit } = req.query;
     res.send(await dbProductManager.getProducts(limit ? limit : 10));
@@ -19,14 +19,14 @@ router.get("/db/", async (req, res) => {
   }
 });
 
-router.get("/db/:pid", async (req, res) => {
+router.get("/:pid", async (req, res) => {
   let productId = req.params.pid;
   const product = await dbProductManager.getProductById(productId);
   if (product.error) return res.status(404).send({ error: "404 Not found." });
   res.send({ product });
 });
 
-router.post("/db/", async (req, res) => {
+router.post("/", async (req, res) => {
   let product = req.body;
   const addProductResponse = await dbProductManager.addProduct(product);
   if (addProductResponse.error)
@@ -34,7 +34,7 @@ router.post("/db/", async (req, res) => {
   return res.status(200).send(addProductResponse);
 });
 
-router.put("/db/:pid", async (req, res) => {
+router.put("/:pid", async (req, res) => {
   const productId = req.params.pid;
   const productInfo = req.body;
   const updateProductResponse = await dbProductManager.updateProduct(
@@ -47,65 +47,9 @@ router.put("/db/:pid", async (req, res) => {
   return res.send(updateProductResponse);
 });
 
-router.delete("/db/:pid", async (req, res) => {
+router.delete("/:pid", async (req, res) => {
   const productId = req.params.pid;
   const deleteProductResponse = await dbProductManager.deleteProduct(productId);
-  if (deleteProductResponse.error)
-    return res.status(deleteProductResponse.error).send(deleteProductResponse);
-  return res.send(deleteProductResponse);
-});
-
-//*********************
-//***FILESYSTEM API:***
-//*********************
-
-router.get("/fs/", async (req, res) => {
-  let { limit } = req.query;
-  const products = await productManager.getProducts();
-  if (!limit) return res.send({ products });
-  res.send({ products: products.splice(0, parseInt(limit)) });
-});
-
-router.get("/fs/:pid", async (req, res) => {
-  let productId = req.params.pid;
-  const product = await productManager.getProductById(parseInt(productId));
-  if (product.error) return res.sendStatus(product.error);
-  res.send({ product });
-});
-
-router.post("/fs/", async (req, res) => {
-  let product = req.body;
-  const addProductResponse = await productManager.addProduct(
-    product.title,
-    product.description,
-    product.price,
-    product.thumbnail,
-    product.code,
-    product.stock,
-    product.status,
-    product.category
-  );
-  if (addProductResponse.error)
-    return res.status(addProductResponse.error).send(addProductResponse);
-  return res.status(200).send(addProductResponse);
-});
-
-router.put("/fs/:pid", async (req, res) => {
-  const productId = parseInt(req.params.pid);
-  const productInfo = req.body;
-  const updateProductResponse = await productManager.updateProduct(
-    productId,
-    productInfo
-  );
-  if (updateProductResponse.error)
-    return res.status(updateProductResponse.error).send(updateProductResponse);
-
-  return res.send(updateProductResponse.message);
-});
-
-router.delete("/fs/:pid", async (req, res) => {
-  const productId = parseInt(req.params.pid);
-  const deleteProductResponse = await productManager.deleteProduct(productId);
   if (deleteProductResponse.error)
     return res.status(deleteProductResponse.error).send(deleteProductResponse);
   return res.send(deleteProductResponse);
